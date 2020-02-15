f601a979a39d7eae8fbd5388f0d1e5552f1cdfb6
Logan Duffley <Lduffley12@gmail.com>
2020-02-15 16:14:33 -0600

    Added From Validation

--- form validator/script.js
+++ form validator/script.js
@@ -15,14 +15,58 @@ function showError(input, message){
     small.innerText = message
 }
 
+//Success outline
+function showSuccess(input){
+    const formControl = input.parentElement;
+    formControl.className='form-control success'
+}
+
+function checkEmail(input) {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if(re.test(input.value.trim())){
+        showSuccess(input)
+    } else {
+        showError(input, 'Email is not valid')
+    }
+}
+
+//Check required fields
+
+function checkRequired(inputArr){
+    inputArr.forEach(function(input){
+        if(input.value.trim() === ''){
+            showError(input, `${getFieldName(input)} Is Required`)
+        } else{
+            showSuccess(input)
+        }
+    })
+}
+
+//Check input Length
+function checkLength(input, min, max){
+    if(input.value.length < min){
+        showError(input `${getFieldName(input)} must be at least ${min} characters`)
+    } else if(input.value.length > max){
+        showError(input `${getFieldName(input)} must be at less than ${max} characters`)
+    } else{
+        showSuccess(input)
+    }
+}
+
+//Get FieldName
+
+function getFieldName(input){
+    return input.id.charAt(0).toUpperCase() + input.id.slice(1)
+}
+
 
 // Event Listeners
 form.addEventListener('submit', function(e){
     e.preventDefault()
 
-    if (username.value === ''){
-        showError(username, 'Username is required')
-    } else {
-        showSuccess(username)
-    }
+    checkRequired([username, email, password, password2])
+    checkLength(username, 3, 15)
+    checkLength(password, 6, 25)
+    checkEmail(email)
+
 })
